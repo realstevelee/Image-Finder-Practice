7670455ddf76685312b40d2a53bb82b57b8e44a2
realstevelee <realstevelee@gmail.com>
2020-09-01 17:12:30 -0400

    added axios package

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+              query: term  
+            },
+            headers: {
+                Authorization: 'Client-ID TYcT4uNPf5MAA_lik14T_BOuHWfVOFsNY2fu4feP15Q'
+            }
+        });
     }
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: "20px" }}>
